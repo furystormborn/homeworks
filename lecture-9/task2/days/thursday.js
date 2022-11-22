797533eb6cb84109ea47727c41c3e47637557752
Alexey <luke.smerechevskiy.wp@gmail.com>
2022-11-22 16:22:34 +0200

    Lecture 9

---
+++ lecture-9/task2/days/thursday.js
@@ -0,0 +1,7 @@
+const thursday = {
+    1: 'C++',
+    2: 'C#',
+    3: 'C',
+};
+
+export default thursday;
---
+++ lecture-9/task2/days/wednesday.js
@@ -0,0 +1,7 @@
+const wednesday = {
+    1: 'Physics',
+    2: 'Ukrainian',
+    3: 'Javascript',
+};
+
+export default wednesday;
